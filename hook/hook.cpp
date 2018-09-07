4c7323985c652198bdb4e5e8dec33859b47f6f16
Dennis Fischer <dennis.fischer@live.com>
2018-09-07 10:19:57 +0200

    Fixed hook

--- hook/hook.cpp
+++ hook/hook.cpp
@@ -1,19 +1,37 @@
+#include <unordered_set>
+
+bool already_seen_placeholder(long long expected) {
+  static std::unordered_set<long long> placeholders;
+  return !placeholders.insert(expected).second;
+}
+
 extern "C" {
-void guardMe(const unsigned int address, const unsigned int length, const unsigned int expectedHash) {} ;
+void guardMe(const unsigned int address, const unsigned int length, const unsigned int expectedHash) {}
 
-void response() {} ;
+void response() {}
 
-void do_assert(long long *hash, long long expected) {} ;
+void do_assert(long long *hash, long long expected) {
+  response();
+}
 
-void assert(long long *hash, long long expected) {} ;
+void assert(long long *hash, long long expected) {
+  if (already_seen_placeholder(expected)) {
+    return;
+  }
+  do_assert(hash, expected);
+}
 
-void oh_hash1(long long *hashVariable, long long value) {} ;
+void oh_hash1(long long *hashVariable, long long value) {
+  *hashVariable = *hashVariable + value;
+}
 
-void oh_hash2(long long *hashVariable, long long value) {} ;
+void oh_hash2(long long *hashVariable, long long value) {
+  *hashVariable = *hashVariable ^ value;
+}
 
-void registerFunction(char functionName[]) {} ;
+void registerFunction(char functionName[]) {}
 
-void deregisterFunction(const char functionName[]) {} ;
+void deregisterFunction(const char functionName[]) {}
 
-void verifyStack() {} ;
+void verifyStack() {}
 }
